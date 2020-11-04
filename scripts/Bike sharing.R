c924a45bafceeff6fead69ef897e3747a5bf21e6
NDFabri <nannetfabri@gmail.com>
2020-11-04 11:44:45 +0100

    Adding plyr library Renaming the seasons

--- scripts/Bike sharing.R
+++ scripts/Bike sharing.R
@@ -16,10 +16,34 @@ Data <- read_csv("data/train.csv")
 
 ##Check if there is a relationship between count and season####
 
+#Making season as a factor and adding names for each season
+Data$season <- as.character(Data$season)
+
 #Boxplot with counts per season
 CSplot <- ggplot(data = Data, mapping = aes(x = season, y = count, group=season)) +
   geom_boxplot(alpha = 0) + theme_bw()
 CSplot
+
+#Test to see if there is a difference in counts between seasons
+kruskal.test(count ~ season, data = Data)
+#Since the p-value is lower than 0.001 we assume that there is a difference
+#Dunn test as a post-hoc test (to see where the difference is)
+dunn.test(Data$count, Data$season, method = "sidak")
+
+#Adding the differences between seasons in the boxplot
+CSplot <- CSplot + 
+  annotate("text", x=1, y=425, label="a", size=7) +
+  annotate("text", x=2, y=775, label="b", size=7) +
+  annotate("text", x=3, y=800, label="c", size=7) +
+  annotate("text", x=4, y=700, label="b", size=7)
+CSplot
+
+##Check if there is a relationship between count and holiday####
+
+#Boxplot with counts per season
+CHplot <- ggplot(data = Data, mapping = aes(x = holiday, y = count, group=holiday)) +
+  geom_boxplot(alpha = 0) + theme_bw()
+CHplot
 
 #Test to see if there is a difference in counts between seasons
 kruskal.test(count ~ season, data = Data)
