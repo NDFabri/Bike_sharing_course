7a91c1c161e240a6799e4028fa5599cae4718b5a
NDFabri <nannetfabri@gmail.com>
2020-11-05 21:19:27 +0100

    Saving the plots as a jpeg

--- scripts/Bike sharing.R
+++ scripts/Bike sharing.R
@@ -30,11 +30,16 @@ Data$season <- as.character(Data$season)
 Data$season <- revalue(Data$season, c("1"="1:January-March", "2"="2:April-June", "3"="3:July-September", "4"="4:October-December"))
 
 #See if there is an association between count and season
-#And if so, make a boxplot
-bikecourse_function(Data$count, Data$season, data=Data,
+CS <- bikecourse_function(Data$count, Data$season, data=Data,
                     threshold = 0.05,
                     xlab = "season", ylab = "count",
                     yax = 900, size = 7)
+CS #Make a boxplot of the association
+
+#Saving the boxplot as a jpeg
+jpeg(file="plots/count_vs_season.jpeg")
+CS
+dev.off()
 
 ##Check if there is an association between count and holiday####
 
@@ -43,8 +48,7 @@ Data$holiday <- as.character(Data$holiday)
 Data$holiday <- revalue(Data$holiday, c("1"="Yes", "0"="No"))
 
 #See if there is an association between count and holiday
-#And if so, make a boxplot
-bikecourse_function(Data$count, Data$holiday, data=Data,
+CH <- bikecourse_function(Data$count, Data$holiday, data=Data,
                     threshold = 0.05,
                     xlab = "holiday", ylab = "count",
                     yax = 900, size = 7)
@@ -56,8 +60,7 @@ Data$workingday <- as.character(Data$workingday)
 Data$workingday <- revalue(Data$workingday, c("1"="Yes", "0"="No"))
 
 #See if there is an association between count and workingday
-#And if so, make a boxplot
-bikecourse_function(Data$count, Data$workingday, data=Data,
+CW <- bikecourse_function(Data$count, Data$workingday, data=Data,
                     threshold = 0.05,
                     xlab = "workingday", ylab = "count",
                     yax = 900, size = 7)
@@ -68,44 +71,72 @@ bikecourse_function(Data$count, Data$workingday, data=Data,
 Data$weather <- as.character(Data$weather)
 
 #See if there is an association between count and weather
-#And if so, make a boxplot
-bikecourse_function(Data$count, Data$weather, data=Data,
+CWe <- bikecourse_function(Data$count, Data$weather, data=Data,
                     threshold = 0.05,
                     xlab = "weathertype", ylab = "count",
                     yax = 900, size = 7)
+CWe #Make a boxplot of the association
+
+#Saving the boxplot as a jpeg
+jpeg(file="plots/count_vs_weather.jpeg")
+CWe
+dev.off()
 
 ##Check if there is an association between count and temp####
 
 #See if there is an association between count and temp
-#And if so, make a scatterplot with regressionline
-bikecourse_function(Data$count, Data$temp, data=Data,
+CT <- bikecourse_function(Data$count, Data$temp, data=Data,
                     threshold = 0.05,
                     xlab = "temp", ylab = "count",
                     yax = 900, size = 7)
+CT #Make a scatterplot of the association
+
+#Saving the scatterplot as a jpeg
+jpeg(file="plots/count_vs_temp.jpeg")
+CT
+dev.off()
 
 ##Check if there is an association between count and atemp####
 
 #See if there is an association between count and atemp
 #And if so, make a scatterplot with regressionline
-bikecourse_function(Data$count, Data$atemp, data=Data,
+CA <- bikecourse_function(Data$count, Data$atemp, data=Data,
                     threshold = 0.05,
                     xlab = "atemp", ylab = "count",
                     yax = 900, size = 7)
+CA #Make a scatterplot of the association
+
+#Saving the scatterplot as a jpeg
+jpeg(file="plots/count_vs_atemp.jpeg")
+CA
+dev.off()
 
 ##Check if there is an association between count and humidity####
 
 #See if there is an association between count and humidity
 #And if so, make a scatterplot with regressionline
-bikecourse_function(Data$count, Data$humidity, data=Data,
+CHu <-bikecourse_function(Data$count, Data$humidity, data=Data,
                     threshold = 0.05,
                     xlab = "humidity", ylab = "count",
                     yax = 900, size = 7)
+CHu #Make a scatterplot of the association
+
+#Saving the scatterplot as a jpeg
+jpeg(file="plots/count_vs_humidity.jpeg")
+CHu
+dev.off()
 
 ##Check if there is an association between count and windspeed####
 
 #See if there is an association between count and windspeed
 #And if so, make a scatterplot with regressionline
-bikecourse_function(Data$count, Data$windspeed, data=Data,
+CWi <-bikecourse_function(Data$count, Data$windspeed, data=Data,
                     threshold = 0.05,
                     xlab = "windspeed", ylab = "count",
                     yax = 900, size = 7)
+CWi #Make a scatterplot of the assocation
+
+#Saving the scatterplot as a jpeg
+jpeg(file="plots/count_vs_windspeed.jpeg")
+CWi
+dev.off()
